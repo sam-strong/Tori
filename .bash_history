01782c4d17ea4af42b7e6d4f72d4e64db6c9c459
Sam Strong <s.j.p.strong-08@student.lboro.ac.uk>
2013-08-06 14:49:04 +0000

    first commit

---
+++ .bash_history
@@ -0,0 +1,25 @@
+ls
+cd T
+cd ToriHarris/
+subl .
+sublime .
+ls
+app
+git init
+git push
+git add origin https://github.com/sam-strong/Tori.git
+git remote add origin https://github.com/sam-strong/Tori.git
+git push -u origin master
+git push -u origin master
+git push -u origin master
+git remote add origin git@github.com:sam-strong/Tori.git
+git push -u origin master
+git commit
+git config --global user.email "s.j.p.strong-08@student.lboro.ac.uk"
+git commit
+git add .
+git commit
+git config --global user.name "Sam Strong"
+git status
+git add .
+git commit .
---
+++ ToriHarris/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ToriHarris::Application.config.secret_key_base = 'f94e90362a564b294389f15553e992de39eb76482cd3c39bcb30a43291c004e06dcace859b2e423cc3b6b3e82e18cf43be7ebc852bf0ed1a1367c3bd3614d528'
